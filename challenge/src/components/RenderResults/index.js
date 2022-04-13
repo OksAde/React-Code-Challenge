87c227c87af78698bb9708bebdc09dc3f904e1b9
OksAde <oks_99@hotmail.co.uk>
2022-04-13 20:34:08 +0100

    small changes

--- challenge/src/components/RenderResults/index.js
+++ challenge/src/components/RenderResults/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Results({data}) {
   return (
-    <div className='item' role='listItem'>
-        <h3><a href={data.html_url} target="_blank">{data.name}</a></h3>
+    <div className='repoItem' role='listItem'>
+        <h2><a href={data.html_url} target="_blank">{data.name}</a></h2>
             <p>Forks: {data.forks_count}</p>
             <p>Stargazers: {data.stargazers_count}</p>
             <p>Open Issues: {data.open_issues}</p>
