b8d9af4419451665b5fda9d6052789af748b221a
OksAde <oks_99@hotmail.co.uk>
2022-04-13 20:24:02 +0100

    actions

--- challenge/src/actions/index.js
+++ challenge/src/actions/index.js
@@ -1,15 +1,18 @@
 import axios from 'axios';
 
-
-
-
-export const getRepos = username =>  {
-    return async dispatch => {
+export const getRepos = (username) =>  {
+    return async (dispatch) => {
         try {
-            const {data} = await axios.get(`https://api.github.com/users/${username}/repos`)
-            return data
-        } catch(err) {
-            throw new Error(err.message)
+            const datas = await axios.get(`https://api.github.com/users/${username}/repos`)
+            const data = await datas.json();
+            if(!Array.isArray(data)) throw data;
+            dispatch({
+                type: "LOAD_REPOS",
+                payload: data
+        });
+    }catch (err) {
+            console.warn(err.message);
+            dispatch({ type: 'SET_ERROR', payload: err.message })
         }
     }
 }
